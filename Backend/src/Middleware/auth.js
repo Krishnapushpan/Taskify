20a0f000b18eb341e03fe4d13a46133e3687400b
Krishnapushpan <krishnapushpan2003@gmail.com>
2025-05-23 00:27:34 +0530

    updated auth.js

--- Backend/src/Middleware/auth.js
+++ Backend/src/Middleware/auth.js
@@ -4,14 +4,19 @@ dotenv.config();
 
 function verifyToken(req, res, next) {
   // Get token from cookie or Authorization header
-  const token =
-    req.cookies.Authtoken ||
-    (req.headers.authorization && req.headers.authorization.split(" ")[1]);
+  const cookies =req.headers.cookie;
+  const cookie=cookies.split(';')
+  for(let cooki of cookie){
+    const [name,value] = cooki.trim().split('=');
+    if(name=='Authtoken'){
+  // const token =
+  //   req.cookies.Authtoken ||
+  //   (req.headers.authorization && req.headers.authorization.split(" ")[1]);
 
-  if (!token) return res.status(401).json({ error: "Access denied" });
+  // if (!token) return res.status(401).json({ error: "Access denied" });
 
   try {
-    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    const decoded = jwt.verify(value, process.env.SECRET_KEY);
     req.role = decoded.role;
     req.email = decoded.email;
     req.fullName = decoded.fullName;
@@ -22,5 +27,5 @@ function verifyToken(req, res, next) {
     res.status(401).json({ error: "Invalid token" });
   }
 }
-
+  }}
 export { verifyToken };
