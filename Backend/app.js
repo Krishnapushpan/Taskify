4713332827edaf3908be40c15960d201e430d282
Krishnapushpan <krishnapushpan2003@gmail.com>
2025-05-22 10:50:53 +0530

    updated app.js

--- Backend/app.js
+++ Backend/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ['http://localhost:3000', 'https://taskify-swart-six.vercel.app'],
     credentials: true,
   })
 );
