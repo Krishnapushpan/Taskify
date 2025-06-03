25ca80c5d2bee4e0143c23c11dedede124219e94
Krishnapushpan <krishnapushpan2003@gmail.com>
2025-06-03 21:59:04 +0530

    updated app.js

--- Backend/app.js
+++ Backend/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'https://taskify-frontend-g0b4.onrender.com'],
+    origin: ['http://localhost:3000', 'https://taskify-f-t3pg.onrender.com'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
